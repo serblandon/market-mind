7d0fe41b0755edbeea62ac21b32af7cc30e8e6a0
Serban Crasovan <serban.crasovan@student.upt.ro>
2024-02-22 00:21:21 +0200

    Create watchlist component, Route sidebar elements to components

--- frontend/market-mind-ui/src/app/app.routes.ts
+++ frontend/market-mind-ui/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { WatchlistComponent } from './components/watchlist/watchlist.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomePageComponent },
+    { path: 'watchlist', component: WatchlistComponent },
     { path: '**', component: HomePageComponent }
 ];
